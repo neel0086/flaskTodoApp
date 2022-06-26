630c3fd36e207e4d239fae7722c78371468d6224
Neel Mehta <neelmehta0086@gmail.com>
2022-06-26 14:25:39 +0530

    3rd commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import re
 from turtle import title
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
@@ -14,6 +14,7 @@ db=SQLAlchemy(app)
 
 class Todo(db.Model):
     sr_no = db.Column(db.Integer, primary_key = True)
+    todo_user = db.Column(db.String(500), nullable = False)
     title = db.Column(db.String(200), nullable = False)
     desc = db.Column(db.String(500), nullable = False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
@@ -23,7 +24,9 @@ class Todo(db.Model):
 
 class Todocomment(db.Model):
     __bind_key__ = 'todocomment'
+    
     sr_no = db.Column(db.Integer, primary_key = True)
+    todo_user = db.Column(db.String(500), nullable = False)
     todo_id = db.Column(db.Integer, nullable = False)
     desc = db.Column(db.String(500), nullable = False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
@@ -43,25 +46,46 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         passw = request.form['pass']
-        todolog = Todologin(todo_user=username, passw=passw)
-        db.session.add(todolog)
-        db.session.commit()
-        return redirect(url_for('home'))
+        data = Todologin.query.filter_by(todo_user=username, passw=passw).first()
+        if data is not None:
+            session['logged_in'] = True
+            session['user'] = data.todo_user
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html')
+        # todolog = Todologin(todo_user=username, passw=passw)
+        # db.session.add(todolog)
+        # db.session.commit()  
     else:
         return render_template('login.html')
 
+@app.route("/register" ,methods=['GET','POST'])
+def register():
+    
+    if request.method == 'POST':
+        try:
+            utodo=Todologin(todo_user=request.form['username'], passw=request.form['pass'])
+            db.session.add(utodo)
+            db.session.commit()
+            return redirect(url_for('login'))
+        except:
+            return render_template('register.html', message="User Already Exists")
+    else:
+        
+        return render_template('register.html')
 @app.route("/home" ,methods=['GET','POST'])
 def home():
     # print(mssg)
+    user = session['user']
     if request.method == 'POST':
-        # print(request.form['title'])
+        todo_user = user
         title = request.form['title']
         desc = request.form['desc']
-        todo = Todo(title=title, desc=desc)
+        todo = Todo(title=title, desc=desc, todo_user=todo_user)
         db.session.add(todo)
         db.session.commit()
     else:
-        allTodo = Todo.query.all()
+        allTodo = Todo.query.filter_by(todo_user=user).all()
         return render_template('index.html', allTodo=allTodo)
 
 
@@ -90,19 +114,18 @@ def update(sr_no):
 
 @app.route('/comment/<int:sr_no>', methods=['GET', 'POST'])
 def comment(sr_no):
-    
+    user = session['user']
     if request.method=='POST':
         desc = request.form['desc']
-        # todo = Todo.query.filter_by(sr_no=sr_no).first()
-        todo1 = Todocomment(desc=desc, todo_id = sr_no)
+        todo_user = user
+        todo1 = Todocomment(desc=desc, todo_id = sr_no, todo_user=todo_user)
         db.session.add(todo1)
         db.session.commit()
-        # print(todo,99)
-        # return redirect("/")
         
     todo = Todo.query.filter_by(sr_no=sr_no).first()
     comments = Todocomment.query.filter_by(todo_id=sr_no).all()
     return render_template('comment.html', todo=todo, comments=comments)
 
 if __name__ == '__main__':
+    app.secret_key = "SecretCantBeSecret:;lol"
     app.run(debug=True,port=8000)
